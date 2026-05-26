4b2e59e093963b50aee17d434a46c5993f7f1e61
RubenBC <rbcheca@gmail.com>
2026-05-26 10:12:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rubencefit-v4.2';
+const CACHE_NAME = 'rubencefit-v4.3';
 const STATIC_ASSETS = ['./icon.png','./ch3ca-logo.png','./manifest.json'];
 
 self.addEventListener('install', event => {
