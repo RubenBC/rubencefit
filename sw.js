a50ba705fba50ab8645d4c12d0fdf471a3b33d75
RubenBC <rbcheca@gmail.com>
2026-06-06 12:22:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rubencefit-v4.9';
+const CACHE_NAME = 'rubencefit-v5.0';
 const STATIC_ASSETS = ['./icon.png','./ch3ca-logo.png','./manifest.json'];
 
 self.addEventListener('install', event => {
