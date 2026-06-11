e092f38f9ad7324945c75d48d24dc4e60c9e8766
RubenBC <rbcheca@gmail.com>
2026-06-11 10:16:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rubencefit-v2.0';
+const CACHE_NAME = 'rubencefit-v2.1';
 const STATIC_ASSETS = ['./icon.png','./ch3ca-logo.png','./manifest.json','https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.2/Sortable.min.js'];
 
 self.addEventListener('install', event => {
